da8398148d1d179c157514ae0430415547dcc1d7
Vadimas Ko <kozemiakinas123@gmail.com>
2022-08-08 14:37:26 +0300

    Updates

---
+++ precache-manifest.c8561f63820173009f53305b2cc13e1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c082ef6148de71b6be",
+    "url": "/threeJS-earth-globe/static/css/main.0e367a69.chunk.css"
+  },
+  {
+    "revision": "15c082ef6148de71b6be",
+    "url": "/threeJS-earth-globe/static/js/main.15c082ef.chunk.js"
+  },
+  {
+    "revision": "cca9470c38fab98c187b",
+    "url": "/threeJS-earth-globe/static/js/1.cca9470c.chunk.js"
+  },
+  {
+    "revision": "300cf86e5bc6d63fea34",
+    "url": "/threeJS-earth-globe/static/js/runtime~main.300cf86e.js"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/threeJS-earth-globe/static/media/stars.6494ab5b.png"
+  },
+  {
+    "revision": "590a808ab4229608c2de0dbaaf4e454b",
+    "url": "/threeJS-earth-globe/static/media/earthTexture.590a808a.jpg"
+  },
+  {
+    "revision": "88c49ce5b242c79568285c436d4112c3",
+    "url": "/threeJS-earth-globe/static/media/earthElevation.88c49ce5.jpg"
+  },
+  {
+    "revision": "4cd95b4fefd23764524cf7af6f209c02",
+    "url": "/threeJS-earth-globe/index.html"
+  }
+];
